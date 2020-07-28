b01f47b7b15f6f1c8d4099d1ccb04d3abec137b1
ahcorde <ahcorde@gmail.com>
2020-07-28 08:47:11 +0200

    make linters happy
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- gazebo_ros2_control_demos/launch/cart_example_position.launch.py
+++ gazebo_ros2_control_demos/launch/cart_example_position.launch.py
@@ -19,9 +19,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
 
 from launch_ros.actions import Node
 
