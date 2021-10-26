83089d32e5f935d771ffb71e2332956db6acd2b9
Bence Magyar <bence.magyar.robotics@gmail.com>
2021-10-26 08:32:56 +0100

    Galactic: Pass time and period to update function (#88)
    * Pass time and period to update function
    
    * Use Galactic in GH action
    
    * joint_state_broadcaster in town

--- gazebo_ros2_control/src/gazebo_ros2_control_plugin.cpp
+++ gazebo_ros2_control/src/gazebo_ros2_control_plugin.cpp
@@ -369,9 +369,9 @@ void GazeboRosControlPrivate::Update()
   rclcpp::Duration sim_period = sim_time_ros - last_update_sim_time_ros_;
 
   if (sim_period >= control_period_) {
-    last_update_sim_time_ros_ = sim_time_ros;
     controller_manager_->read();
-    controller_manager_->update();
+    controller_manager_->update(sim_time_ros, sim_period);
+    last_update_sim_time_ros_ = sim_time_ros;
   }
 
   // Always set commands on joints, otherwise at low control frequencies the joints tremble
