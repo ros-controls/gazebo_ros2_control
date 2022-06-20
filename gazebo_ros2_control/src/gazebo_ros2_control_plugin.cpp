42d528722ab15070cedb7dac3fcb401a5a793a44
ahcorde <ahcorde@gmail.com>
2022-06-20 14:02:27 +0200

    Added logic for hardware interfaces
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- gazebo_ros2_control/src/gazebo_ros2_control_plugin.cpp
+++ gazebo_ros2_control/src/gazebo_ros2_control_plugin.cpp
@@ -304,6 +304,11 @@ void GazeboRosControlPlugin::Load(gazebo::physics::ModelPtr parent, sdf::Element
     resource_manager_->import_component(std::move(gazeboSystem), control_hardware[i]);
   }
 
+  rclcpp_lifecycle::State state(
+    lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE,
+    hardware_interface::lifecycle_state_names::ACTIVE);
+  resource_manager_->set_component_state("GazeboSimSystem", state);
+
   impl_->executor_ = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
 
   // Create the controller manager
