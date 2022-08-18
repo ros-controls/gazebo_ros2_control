f64e56af4800120150460ffba15cd73db511521c
Tony Najjar <tony.najjar@logivations.com>
2022-08-18 11:00:54 +0200

    Enable loading params from multiple yaml files (#149)

--- gazebo_ros2_control/src/gazebo_system.cpp
+++ gazebo_ros2_control/src/gazebo_system.cpp
@@ -193,8 +193,8 @@ void GazeboSystem::registerJoints(
       }
       RCLCPP_INFO_STREAM(
         this->nh_->get_logger(),
-        "Joint '" << joint_name << "'is mimicing joint '" << mimicked_joint << "' with mutiplier: "
-                  << mimic_joint.multiplier);
+        "Joint '" << joint_name << "'is mimicing joint '" << mimicked_joint <<
+          "' with mutiplier: " << mimic_joint.multiplier);
       this->dataPtr->mimic_joints_.push_back(mimic_joint);
       suffix = "_mimic";
     }
